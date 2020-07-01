ac673321f445a799264a89f814abf35b426897a4
jachoi <jjanghacker@naver.com>
2020-07-02 07:15:48 +0900

    Now PDF files saved in PDFS Folder

--- 42PDFFinder.sh
+++ 42PDFFinder.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
-START_IDX=7800
+
+
+START_IDX=7500
 MAX_IDX=10000
 ITER=$(seq $START_IDX $MAX_IDX)
 LANG=ko
+
+mkdir PDFS
 
 for i in $ITER
 do
@@ -11,9 +15,10 @@ res=$(curl -w '%{http_code}' -s -o /dev/null "https://cdn.intra.42.fr/pdf/pdf/$i
 if [ $res -eq 200  ]
 then
 	echo "https://cdn.intra.42.fr/pdf/pdf/$i/$LANG.subject.pdf"
-	curl -s "https://cdn.intra.42.fr/pdf/pdf/$i/$LANG.subject.pdf" -o "$LANG$i.pdf"
+	curl -s "https://cdn.intra.42.fr/pdf/pdf/$i/$LANG.subject.pdf" -o "./PDFS/$LANG$i.pdf"
 fi
 } &
 done
 
 wait
+echo "Done!"
